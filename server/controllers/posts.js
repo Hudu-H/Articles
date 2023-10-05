9cfc1d073374a9b8ebb2de70b18a799125b03baa
Hudu <ahmedhudu21@gmail.com>
2023-10-05 02:36:54 +0200

    create models/controllers

---
+++ server/controllers/posts.js
@@ -0,0 +1,28 @@
+//this folder will help separate and keep clean our route/posts.js file because of the numerous function calls.
+//https://www.restapitutorial.com/httpstatuscodes.html
+
+//internal imports
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPost = async (req, res) => {
+  const post = req.body;
+
+  const newPost = new PostMessage(post);
+  try {
+    await newPost.save();
+
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,12 @@
 import express from "express";
 
+//internal imports
+import { getPosts, createPost } from "../controllers/posts.js";
+
 const router = express.Router();
 
 //http://localhost:5000/posts
-router.get("/", (req, res) => {
-  res.send("It worked!");
-});
+router.get("/", getPosts);
+router.post("/", createPost);
 
 export default router;
