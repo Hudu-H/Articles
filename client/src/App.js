be9d1f497256ee4ae7e0372cfc4edd0c63f0cfe9
Hudu <ahmedhudu21@gmail.com>
2023-10-06 22:53:22 +0200

    create container/appBar for posts and form

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
-import { Container, AppBar, Typography } from "@mui/material";
-import scenic from './images/scenic.png';
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import scenic from "./images/scenic.png";
 const App = () => {
   return (
     <Container maxWidth="lg">
       <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">Scenic_Places</Typography>
-        <img  src={scenic} alt="scenic_places" height="60" width={40}/>
+        <Typography variant="h2" align="center">
+          Scenic_Places
+        </Typography>
+        <img src={scenic} alt="scenic_places" height="60" />
       </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              {/* <Posts /> */}
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              {/* <Form /> */}
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
     </Container>
   );
 };
