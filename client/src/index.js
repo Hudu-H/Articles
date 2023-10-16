e19238ecbd623e436ae1d633d9efe8b5521117c5
Hudu <ahmedhudu21@gmail.com>
2023-10-17 01:37:28 +0200

    fixed changes

--- client/src/index.js
+++ client/src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import { configureStore, applyMiddleware, compose } from "@reduxjs/toolkit";
-import reducers from "./reducers";
 import thunk from "redux-thunk";
 
-export const store = configureStore(
-  { reducer: reducers },
-  compose(applyMiddleware(thunk))
-);
+import reducers from "./reducers";
+
+export const store = configureStore(reducers, compose(applyMiddleware(thunk)));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
--- server/index.js
+++ server/index.js
@@ -10,15 +10,15 @@ dotenv.config();
 
 const app = express();
 
+app.use("/posts", postRoutes);
+
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use("/posts", postRoutes);
-
-const CONNECTION_URL =
-  "mongodb+srv://ahmedhudu21:2snakebite@cluster0.opwt0ug.mongodb.net/?retryWrites=true&w=majority";
+const CONNECTION_URL = process.env.MONGO_APIKEY;
 const PORT = process.env.PORT || 5000;
+
 
 //accepts two different parameters before but not now
 mongoose
