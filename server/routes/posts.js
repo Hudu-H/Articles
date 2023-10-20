c9ff0e7552062c22178e9e426f0bfa9d380e169e
Hudu <ahmedhudu21@gmail.com>
2023-10-20 02:32:47 +0200

    create delete post api

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,13 +1,14 @@
 import express from "express";
 
 //internal imports
-import { getPosts, createPost , updatePost} from "../controllers/posts.js";
+import { getPosts, createPost, updatePost, deletePost } from "../controllers/posts.js";
 
 const router = express.Router();
 
 //http://localhost:5000/posts
 router.get("/", getPosts);
 router.post("/", createPost);
-router.patch('/:id', updatePost)
+router.patch("/:id", updatePost);
+router.delete("/:id", deletePost);
 
 export default router;
