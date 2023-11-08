c9a6566e67f8103dbf9b7b17a9786d1ad9c8a5be
Hudu <ahmedhudu21@gmail.com>
2023-11-08 22:12:53 +0100

    implement bcrypt for logins

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 
 //internal imports
 import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
 import dotenv from "dotenv";
 
 
@@ -16,6 +17,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
+app.use('/user', userRoutes);
 
 const CONNECTION_URL = process.env.MONGODB_URL;
 const PORT = process.env.PORT;
