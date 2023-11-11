60c693892a3933918cf2002b668154a1685721e8
Hudu <ahmedhudu21@gmail.com>
2023-11-11 22:34:12 +0100

    create user schema

--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,10 @@
 import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { String, required: true },
+  name: { type: String },
+  email: { type: String },
+  password: { String },
   id: { type: String },
 });
 
-
-export default mongoose.model('User', userSchema)
+export default mongoose.model("User", userSchema);
