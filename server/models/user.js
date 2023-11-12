1fa4f4d400ffc1e7449c72c001cb168c8b868f7d
Hudu <ahmedhudu21@gmail.com>
2023-11-12 23:07:38 +0100

    fix schema validation

--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = mongoose.Schema({
-  name: { type: String },
-  email: { type: String },
-  password: { String },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
   id: { type: String },
 });
+
 
 export default mongoose.model("User", userSchema);
