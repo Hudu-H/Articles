20bc24fca43b7a4a23577835f3fa6623b3784f8a
Hudu <ahmedhudu21@gmail.com>
2024-01-02 23:58:29 +0100

    create comment section for post

---
+++ client/src/components/PostDetails/CommentSection.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useRef } from "react";
+import { Typography, TextField, Button } from "@material-ui/core";
+import { useDispatch } from "react-redux";
+
+//internal styles
+import useStyles from "./styles";
+
+const CommentSection = () => {
+  console.log("comment section");
+
+  return <h1>Comment section</h1>;
+};
+
+export default CommentSection;
