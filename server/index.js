d7e35e8fdf2d4b593cbd7957c6c6b283f3c7fab1
Hudu <ahmedhudu21@gmail.com>
2024-12-12 21:25:18 +0100

    review code

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
 
 const CONNECTION_URL = process.env.MONGODB_URL;
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 mongoose
   .connect(CONNECTION_URL)
